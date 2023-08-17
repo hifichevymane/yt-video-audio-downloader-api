b43aa2dbf7a437dc981a74627c75ffa7bf048580
hifichevymane <blekbjek@gmail.com>
2023-08-17 13:22:26 +0300

    Reorganize project. Add CORS and static files dir

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from schemas import YouTubeURL
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import os
 import utils
+from pydantic import BaseModel
+import uvicorn
+
+
+# Schema
+class YouTubeURL(BaseModel):
+    url: str
 
 
 app = FastAPI()
+
+# Creating static files directory
+app.mount('/uploads', StaticFiles(directory='uploads'), name='uploads')
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Directory to upload all videos
 UPLOAD_DIR = os.path.join(BASE_DIR, 'uploads')
+
+# Allow React app to send API requests
+ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    "localhost:3000",
+]
+
+# Adding CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Downloading a video
@@ -16,12 +41,17 @@ UPLOAD_DIR = os.path.join(BASE_DIR, 'uploads')
 def get_video(video_url: YouTubeURL):
     file_info = utils.download_video(video_url=video_url.url)
     # Returning a download link
-    return FileResponse(path=file_info.path, filename=file_info.name)
+    return {'file_path': f'http://127.0.0.1:8000/uploads/{file_info.name}'}
 
 
 # Downloading a audio
 @app.post('/api/get_audio/')
 def get_audio(video_url: YouTubeURL):
     file_info = utils.download_audio(video_url=video_url.url)
-    # Returning a download link
-    return FileResponse(path=file_info.path, filename=file_info.name)
+    # Returning an answer with download link
+    return {'file_path': f'http://127.0.0.1:8000/uploads/{file_info.name}'}
+
+
+# Running uvicorn server when run the script
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
--- schemas.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class YouTubeURL(BaseModel):
-    url: str
